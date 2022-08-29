c530466df2ce5b94e260dd7ca028eba7a334fb63
Gold-MineScore <96890040+DevMizeKR@users.noreply.github.com>
2022-08-29 17:22:12 +0900

    feat: Created json model

---
+++ HallofRhythm/HallofRhythm/Data/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  HallofRhythm
+//
+//  Created by HWANG-C-K on 2022/08/29.
+//
+
+import Foundation
+
+struct Game: Codable {
+    var gameName: String
+    var imageName: String
+}

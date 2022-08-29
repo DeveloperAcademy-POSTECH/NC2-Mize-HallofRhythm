99aae7ea5c68912a5a169988ba53ce0ffb4d8a89
Gold-MineScore <96890040+DevMizeKR@users.noreply.github.com>
2022-08-29 16:41:33 +0900

    feat: MainCollectionViewCell 추가

--- HallofRhythm/HallofRhythm/MainViewController.swift
+++ HallofRhythm/HallofRhythm/MainViewController.swift
@@ -26,7 +26,7 @@ class MainViewController: UIViewController {
         view.contentInset = .zero
         view.backgroundColor = .systemGray6
         view.clipsToBounds = true
-//        view.register(albumImageCell.self, forCellWithReuseIdentifier: "albumImageCell")
+        view.register(mainImageCell.self, forCellWithReuseIdentifier: "albumImageCell")
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
@@ -44,9 +44,42 @@ class MainViewController: UIViewController {
             collectionView.topAnchor.constraint(equalTo: view.topAnchor),
             collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
-//        collectionView.dataSource = self
-//        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
 }
 
+extension MainViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    //CollectionView Cell의 Size 지정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout:UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize{
+        guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else { fatalError() }
+        let cellColumns = 3
+        let widthOfCells = collectionView.bounds.width
+        let widthOfSpacing = CGFloat(cellColumns - 1) * flowLayout.minimumInteritemSpacing
+        let width = (widthOfCells-widthOfSpacing) / CGFloat(cellColumns)
+        return CGSize(width: width, height: width)
+    }
+    
+//    //CollectionView에 표시되는 Item의 수
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return CoreDataManager.shared.albumImageArray!.count
+//    }
+//
+//    //CollectionView의 각 cell에 이미지 표시
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: albumImageCell.id, for: indexPath) as! albumImageCell
+//        cell.prepare(image:UIImage(data: CoreDataManager.shared.albumImageArray!.reversed()[indexPath.item].value(forKey: "thumbnail") as! Data))
+//        return cell
+//      }
+}
+
+////CollectionView의 이미지 클릭시 CellDetailView 표시
+//extension MainViewController: UICollectionViewDelegate {
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        let cellDetailVC = CellDetailViewController()
+//        cellDetailVC.getindex = indexPath.item
+//        cellDetailVC.getimage = UIImage(data: CoreDataManager.shared.albumImageArray!.reversed()[indexPath.item].value(forKey: "image") as! Data)
+//        self.navigationController?.pushViewController(cellDetailVC,animated: true)
+//    }
+//}
